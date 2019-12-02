9614b602aa57ef85f0ed7f09f1c85de2d087fa09
martingess <martingess@protonmail.ch>
2019-12-02 18:54:24 +0200

    2 homework

--- homework2/11/script.js
+++ homework2/11/script.js
@@ -1,3 +1,20 @@
 var userInput = prompt('Введите данные');
 var userNumber = parseInt(userInput);
-var userFormat = String(userNumber)-user
+var userString = userInput.slice(-1);
+var result = 0;
+
+if (userString==='h'){
+    result = userNumber*60*60;
+}
+if (userString==='d'){
+    result = userNumber*24*60*60;
+}
+if (userString==='w'){
+    result = userNumber*7*24*60*60;
+
+}
+if (userString==='m'){
+    result = userNumber*30*24*60*60;
+}
+
+console.log(result + " секунд");
