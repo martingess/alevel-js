bf1fdd324a549d54c7540dc82055b5753c81dbf6
martingess <martingess@protonmail.ch>
2019-12-05 13:29:53 +0200

    22 tasks done

---
+++ homework3/22/script.js
@@ -0,0 +1,6 @@
+//22. Дана строка 'я учу javascript!'. С помощью метода split запишите каждый символ этой строки в отдельный элемент массива.
+
+let str = 'я учу javascript!';
+let arrayOfLetters = str.split('');
+console.log(arrayOfLetters)
+
