6fa33cee21bbd9a733793e36c96c2f58becc147f
martingess <martingess@protonmail.ch>
2019-12-06 22:26:04 +0200

    3 homework done

--- homework3/27/script.js
+++ homework3/27/script.js
@@ -1,7 +1,7 @@
 //27. Напишите функция, которая принимает аргументом строку и преобразуйте например 'var_test_text' в 'varTestText'. Функция, конечно же, должен работать с любыми аналогичными строками.
-function foo(string) {
-    let newString = string.split('_').join('');
-    return newString;
+function bar(string) {
+    let newString = string.split('_').reduce((priviousValue, currentValue) =>
+        priviousValue + currentValue[0].toUpperCase() + currentValue.slice(1)
+    )
+    console.log(newString)
 }
-
-console.log(foo('var_test_text'))
