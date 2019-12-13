76025431e3f5ef42cf1b02ed7896dfda80375045
martingess <martingess@protonmail.ch>
2019-12-13 18:05:45 +0200

    logic of scope

--- test/darts/main.js
+++ test/darts/main.js
@@ -41,28 +41,3 @@ document.body.addEventListener('mousemove', (event) => { //отслеживае�
     scope.mousePosX = event.clientX;
     scope.mousePosY = event.clientY;
 });
-
-// function moveScope() {
-//     let scope.stepY = 1
-//     let scope.stepX = 1
-//     let move = setInterval(() => {
-//         scope.scopePassedX += scope.stepX;
-//         scope.scopePassedY += scope.stepY;
-//         updateScopePosition(scope.stepX, scope.stepY)
-//         if (scope.scopePassedY > 50 || scope.scopePassedY < -50) {
-//             scope.scopePassedY = scope.scopePassedY > 0 ? 50 : -50
-//             scope.stepY = -scope.stepY;
-//         }
-//         if (scope.scopePassedX > 60 || scope.scopePassedX < -60) {
-//             scope.scopePassedX = scope.scopePassedX > 0 ? 60 : -60
-//             scope.stepX = -scope.stepX;
-//         }
-//     }, 10)
-
-//     function updateScopePosition(x, y) {
-//         let positionNowX = parseInt(scopeImg.style.left);
-//         let positionNowY = parseInt(scopeImg.style.top);
-//         scopeImg.style.left = positionNowX + x + 'px';
-//         scopeImg.style.top = positionNowY + y + 'px';
-//     }
-// }
