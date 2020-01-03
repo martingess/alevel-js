44f5f89c10ac468b7a8df0097ea43c5d6210f93d
martingess <martingess@protonmail.ch>
2020-01-03 12:46:28 +0200

    missed task added

--- homework9/13/main.js
+++ homework9/13/main.js
@@ -1,2 +1,21 @@
-//14. Дан ol. По нажатию на кнопку получите его последнего потомка и удалите его.
+// 13. Даны элементы с классом www$, $ - это инкремент. Добавьте каждому элементу в конец название его тега в нижнем регистре при нажатии. Каждый раз.
 
+const ol = document.getElementsByTagName('ol')[0];
+
+
+function getIterableClassList(className) {
+    const listOfIterableClasses = [];
+
+    for (let i = 1; i < Infinity; i++) {
+        let currentElem = document.getElementsByClassName(`${className}${i}`)[0]
+        if (currentElem) {
+            listOfIterableClasses.push(currentElem);
+        } else {
+            return listOfIterableClasses;
+        }
+    }
+}
+
+const listOfClasses = getIterableClassList('www')
+
+listOfClasses.forEach(elem => elem.onclick = () => elem.textContent = elem.tagName.toLowerCase())
